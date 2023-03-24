5c0dd2a297917df5cf9395b9332a1b9fea4140e6
Qammar <qammarsahu@gmail.com>
2023-03-24 06:23:18 +0500

    Modified

--- cdrm/website/forms.py
+++ cdrm/website/forms.py
@@ -7,3 +7,7 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email  Address'}))
     first_name = forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
     last_name = forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email','password1','passsword2')
